eccbfadb937afba476d65551ec653f1654227a73
Mikhail Shevchuk <cp91@mail.ru>
2025-04-06 00:13:34 +0700

    Now we dont see fruits for itself, only with detector

--- Assets/Scripts/LD/Fruit.cs
+++ Assets/Scripts/LD/Fruit.cs
@@ -1,6 +1,28 @@
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Fruit : MonoBehaviour
 {
     public float EnergyRestore;
+
+    [SerializeField]
+    private Image image;
+
+    public void Start()
+    {
+        var color = image.color;
+        color.a = 0.0f;
+        image.color = color;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        image.DOFade(1.0f, 0.5f);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        image.DOFade(0.0f, 0.5f);
+    }
 }
